b515e56131e6528421c3da83631787f01fbf60f1
Michael <michael.awanayah@gmail.com>
2026-01-17 16:33:40 +0000

    made modifications to views, models and urls

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pages.urls')),
+    path('blog/', include('blog.urls')),
+    path('testimonials/', include('testimonials.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
