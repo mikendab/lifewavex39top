cf4765cc588b13865d478313e1899dfe7caf998a
Michael <michael.awanayah@gmail.com>
2026-01-17 16:43:50 +0000

    added hope page- dynamic with CTA Button

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Post
+from testimonials.models import Testimonial
+from django.shortcuts import render
+
+def home(request):
+    blogs = Post.objects.filter(published=True)[:3]
+    testimonials = Testimonial.objects.filter(approved=True)[:3]
+    return render(request, 'pages/home.html', {
+        'blogs': blogs,
+        'testimonials': testimonials
+    })
+CTA Button:
+<a href="https://lifewave.com/mikendab" class="btn btn-primary btn-lg">
+Order Lifewave X39
+</a>
