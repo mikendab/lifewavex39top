8102c7be08b8a871b4c7dfada79be5c22eabcdbd
Michael <michael.awanayah@gmail.com>
2026-01-20 09:14:11 +0000

    database postgresql

--- config/settings.py
+++ config/settings.py
@@ -103,14 +103,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #       'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+import dj_database_url
+import os
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=True
     )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
