989b9010ae76a8f015854c6a767b731bd7bef322
Michael <michael.awanayah@gmail.com>
2026-01-21 11:30:34 +0000

    ensure user logs out and redirects to homepage

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import signup
+from .views import signup, user_logout
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
+    path('logout/', user_logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import login, logout
 
 def signup(request):
     form = UserCreationForm(request.POST or None)
     if form.is_valid():
-        form.save()
+        user = form.save()
+        messages.success(request, f'Account created successfully for {user.username}! Please log in.')
         return redirect('login')
     return render(request, 'registration/signup.html', {'form': form})
 
-from django.contrib import messages
-from django.contrib.auth import login
-from django.shortcuts import redirect, render
 from django.contrib.auth.forms import AuthenticationForm
+
 def user_login(request):
     form = AuthenticationForm(data=request.POST or None)
     if form.is_valid():
@@ -23,10 +21,14 @@ def user_login(request):
 
         messages.success(
             request,
-            f" Welcome back, {user.username}!"
+            f"Welcome back, {user.username}!"
         )
 
         return redirect('/')
 
     return render(request, 'registration/login.html', {'form': form})
 
+def user_logout(request):
+    logout(request)
+    messages.info(request, "You have been logged out successfully.")
+    return redirect('/')
