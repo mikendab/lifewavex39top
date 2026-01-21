82d64fc89716b373d4fa43664d8c8e23b46e8e0b
Michael <michael.awanayah@gmail.com>
2026-01-21 11:00:07 +0000

    updated bugs in home.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import get_object_or_404, render
+from .models import Post
+
+def blog_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk, published=True)
+    return render(request, 'blog/blog_detail.html', {'post': post})
