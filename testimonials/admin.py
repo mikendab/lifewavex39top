738dacb1ab74d4101927dbf4cac94c56bb7d4238
Michael <michael.awanayah@gmail.com>
2026-01-21 11:58:20 +0000

    testimonial model added to admin panel

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Testimonial
 
-# Register your models here.
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ['user', 'rating', 'approved', 'created']
+    list_filter = ['approved', 'created', 'rating']
+    search_fields = ['user__username', 'content']
+    list_editable = ['approved']
+    readonly_fields = ['user', 'created']
