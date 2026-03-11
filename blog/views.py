349a9c486e5b593817b9ff03f90b5d19c95906de
Michael <michael.awanayah@gmail.com>
2026-03-11 00:35:28 +0000

    pictures of product x39

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 
+
+def home(request):
+    posts = Post.objects.filter(published=True).order_by('-created')
+    return render(request, 'blog/blog_list.html', {'posts': posts})
+
 def blog_detail(request, pk):
     post = get_object_or_404(Post, pk=pk, published=True)
     return render(request, 'blog/blog_detail.html', {'post': post})
